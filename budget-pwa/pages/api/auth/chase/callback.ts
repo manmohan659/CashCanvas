caf143b8206ecf655d7e2a9ffcfae09b4d1d5f37
Manmohan Sharma <manmohan659@gmail.com>
2025-06-10 22:05:47 -0400

    made a lot of changes

--- budget-pwa/pages/api/auth/chase/callback.ts
+++
@@ -1,9 +0,0 @@
-// TODO: Implement Chase OAuth callback handler
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { code } = req.query;
-  // TODO: Exchange authorization code for consent token
-  // TODO: Store access_token (consentToken) in Supabase
-  res.status(200).json({ message: 'Chase OAuth callback placeholder', code });
-}
--- budget-pwa/pages/api/auth/chase/index.ts
+++
@@ -1,8 +0,0 @@
-// TODO: Implement redirect to Chase OAuth
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // TODO: Construct Chase OAuth URL and redirect
-  const chaseOAuthUrl = `https://api.chase.com/aggregator-oauth/authorize?response_type=code&client_id=${process.env.CHASE_CLIENT_ID}&redirect_uri=YOUR_CALLBACK_URL&scope=ais:consents:AccountAggregation`;
-  res.redirect(302, chaseOAuthUrl);
-}
